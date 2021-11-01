eaaf609e76a4be5287d8271f6bda219168382e29
Aishwarya Shrestha <aishwaryashrestha6094@gmail.com>
2021-10-31 21:49:51 -0400

    Added Initials Storyboard

---
+++ ShoppingApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingApplication
+//
+//  Created by Aishwarya Shrestha on 2021-10-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleName: UITextField!
+    @IBOutlet weak var titleNameLabel: UILabel!
+
+    //items quantity
+    @IBOutlet weak var itemsQuantity1: UILabel!
+    @IBOutlet weak var itemsQuantity2: UILabel!
+    @IBOutlet weak var itemsQuantity3: UILabel!
+    @IBOutlet weak var itemsQuantity4: UILabel!
+    @IBOutlet weak var itemsQuantity5: UILabel!
+    
+    
+    
+}
+
